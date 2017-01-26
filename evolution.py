58ef330821e0d07a11ab70935c5ed325dfdde29c
Maciej Pfutzner <maciej.pfutzner.12@ucl.ac.uk>
2017-01-26 23:31:16 +0000

    Incorporate state visualisation into evolution
    Modify evolution module to save simulation states and visualise the
    winner's race with the new module instead of old test one. Also add a
    docstring.

--- evolution.py
+++ evolution.py
@@ -1,7 +1,27 @@
+"""
+Top level module running the main game function. Creates the
+initial fleet of vehicles and a track, calls the simulation to
+perform individual races, evaluates and breeds the winners. Loop is
+repeated a set number of times, with visual displays of the best
+organisms.
+
+Usage: Currently just run the main function (automatic if run as a script),
+it will perform 10 iterations of 100 races and show the winner (press Enter
+to start, Esc to end), while printing some debug messages. The visualisation
+uses a test module and basically reruns the race simulation.
+
+TODO: 
+    Make an interactive game menu allowing to choose options for vehicles,
+    tracks, and other parameters. If possible, allow to visualise any chosen
+    race.
+"""
+import copy
+
 import run_simulation as sim
 import track_generator
 import vehicle
-import test
+#import test
+import visualise
 
 VehicleClass = vehicle.Car # or vehicle.Car
 
@@ -23,8 +43,9 @@ def main():
         for car in fleet:
             #play(car, track)
             sim.setup_sim(car, track)
-            distance, bb, time = sim.run_sim(1e4)
+            distance, bb, time = sim.run_sim(1e4, save=True)
             car.score = distance #if distance < 20 else distance/time*100
+            car.history = copy.copy(sim.history) # FIXME: huge amounts of memory???
             #if distance > max_score:
             #    max_score = distance
 
@@ -45,10 +66,15 @@ def main():
                 fleet.append(winner.get_child())
 
         #print ""
-        play(winners[0], track)
+        #play(winners[0], track)
+        visualise.start_game()
+        visualise.run(winners[0].history, speed=3)
 
 
 def play(car, track):
     test.start_game(car, track)
     test.run(1)
 
+
+if __name__ == '__main__':
+    main()
