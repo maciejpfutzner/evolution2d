5015959fdb5d27fc9aad2efa7e94cb561f11f0dc
Maciej Pfutzner <maciej.pfutzner.12@ucl.ac.uk>
2017-02-01 15:33:45 +0000

    Implement timelines in StateHistory
    Make StateHistory store a single track per object but potentially
    multiple histories of dynamic objects, each with their own tracker and
    shape collection. Access to those changes - now have to retrieve the
    timeline by name ('timeline' by default). StateHistory stores lenght of
    the longest timeline, but no mechanism to stop from running out of
    bounds for shorter ones.

--- state_history.py
+++ state_history.py
@@ -8,7 +8,6 @@ Additional helper functions serve to extract the transformed shapes from body
 attributes.
 
 TODO:
-    Many named timelines, of different lengths
     Proper saving to files
     Store in a cleverer way, ideally just base shapes and transforms
 """
@@ -17,6 +16,7 @@ import shelve
 import Box2D  # The main library
 # Box2D.b2 maps Box2D.b2Vec2 to vec2 (and so on)
 from Box2D.b2 import (world, polygonShape, circleShape, edgeShape, vec2)
+from collections import namedtuple
 
 
 # TODO: a single state should also be a class instead of collection of lists
@@ -34,33 +34,63 @@ class StateHistory:
         Create state history
 
         Arguments:
-            name:   identifier for finding in save files
-            track:  object with a list of static bodies in track.bodies
+            track:  Object with a list of static bodies in track.bodies
+            name:   Identifier for finding in save files
         """
         self.name = name
-        self.track = self.get_shapes(track)
-        self.vehicle_states = []
-        self.tracker_states = []
+        if track:
+            self.track = get_shapes(track)
+        else:
+            self.track = None
+        self.timelines = {}
 
-    def save_state(self, vehicle, timeline='timeline'):
+    def set_track(self, track):
         """
-        Save current state of bodies in vehicle
+        Set track if not given at construction (or reset)
+        """
+        self.track = get_shapes(track)
+
+    Timeline = namedtuple('timeline', ['vehicle', 'vehicle_states',
+        'tracker_states'])
+
+    def new_timeline(self, vehicle, name='timeline'):
+        if name in self.timelines:
+            print 'Warning, overwriting existing timeline "%s"' %name
+        timeline = self.Timeline(vehicle, [], [])
+        self.timelines[name] = timeline
+        return timeline
+
+    def save_state(self, vehicle, timename='timeline'):
+        """
+        Save current state of vehicle bodies. Can also call as a method of a
+        timeline without the vehicle argument.
 
         Arguments:
-            vehicle:    object with list of dynamic bodies in vehicle.bodies
-            timeline:   NOT IMPLEMENTED!!! name of timeline
+            vehicle:    Object with list of dynamic bodies in vehicle.bodies
+            timename:   Name of the timeline
         """
-        shapes = self.get_shapes(vehicle)
-        self.vehicle_states.append(shapes)
+        if timename not in self.timelines:
+            self.new_timeline(vehicle, timename)
+        shapes = get_shapes(vehicle)
+        self.timelines[timename].vehicle_states.append(shapes)
         tracker = vec2(vehicle.tracker)
-        self.tracker_states.append(tracker)
+        self.timelines[timename].tracker_states.append(tracker)
 
-    def get_shapes(self, instance):
-        shapes = []
-        for body in instance.bodies:
-            for fixture in body:
-                shapes.append( fixture.shape.get_transformed_shape(body) )
-        return shapes
+    def _save_state_timeline(timeline):
+        """
+        Alternative way of saving a state, called as a method of a timeline
+        """
+        shapes = get_shapes(timeline.vehicle)
+        timeline.vehicle_states.append(shapes)
+        tracker = vec2(timeline.vehicle.tracker)
+        timeline.tracker_states.append(tracker)
+    Timeline.save_state = _save_state_timeline
+
+    @property
+    def max_length(self):
+        if self.timelines:
+            return max([len(self.timelines[key].vehicle_states) for key in
+                                self.timelines])
 
     def write_to_file(self, filename):
         """
@@ -81,6 +111,14 @@ class StateHistory:
         """
         pass
 
+
+# Module function to get the shapes from bodies
+def get_shapes(instance):
+    shapes = []
+    for body in instance.bodies:
+        for fixture in body:
+            shapes.append( fixture.shape.get_transformed_shape(body) )
+    return shapes
 
 # Helper functions to extract shapes in their correct positions
 #   Add them as methods to the shape classes for 'polymorphic' calls
